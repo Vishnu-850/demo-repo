fb4a6e70226b75240b1690aaf4313ba658701e0f
vishnu <vishnumalothu04@gmail.com>
2025-10-16 22:56:36 +0530

    Add pair.cpp

---
+++ pair.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+	pair<int,int> p={2,3};
+	cout<<p.first<<" "<<p.second<<endl;
+	pair<int,pair<int,int>> x ={1,{3,4}};
+	cout<<x.first<<" "<<x.second.first<<" "<<x.second.second<<endl;
+	pair<string,string> V[]={
+		{"vishnu","arham"},{"vishnutha","vishnuu"},{"varshith","veeksha"}
+		
+	};
+	cout<<V[1].first<<endl;
+	
+}
+ 
